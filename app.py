c5767fbd499d8966049336f8ce8df28baf18dea8
arolobrancalhao <aroldobrancalhaojunior@gmail.com>
2024-07-22 17:11:49 -0300

    atualizado app.py

--- app.py
+++ app.py
@@ -17,10 +17,16 @@ def home():
 def predict():
     try:
         # Recebe os dados do formulário
-        sepal_length = float(request.form['sepal_length'])
-        sepal_width = float(request.form['sepal_width'])
-        petal_length = float(request.form['petal_length'])
-        petal_width = float(request.form['petal_width'])
+        sepal_length = request.form['sepal_length'].replace(',', '.')
+        sepal_width = request.form['sepal_width'].replace(',', '.')
+        petal_length = request.form['petal_length'].replace(',', '.')
+        petal_width = request.form['petal_width'].replace(',', '.')
+
+        # Converte as strings para float
+        sepal_length = float(sepal_length)
+        sepal_width = float(sepal_width)
+        petal_length = float(petal_length)
+        petal_width = float(petal_width)
         
         # Verifica se as características têm o tamanho correto
         features = [sepal_length, sepal_width, petal_length, petal_width]
